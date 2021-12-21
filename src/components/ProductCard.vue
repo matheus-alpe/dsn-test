1cdcd7e10aed75f0c6a257f6c2276d329cd42db0
Matheus Pereira <matttalves@gmail.com>
2021-12-21 17:03:06 -0300

    feat: base structure

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="product">
+    
+    <div class="thumbnail">
+      <img :src="product.image_url" :alt="product.name" referrerpolicy="no-referrer"/>
+    </div>
+
+    <div class="content">
+      <p class="name">{{ product.name }}</p>
+
+      <div class="prices">
+        <span class="oldprice">{{ formatPrice(product.oldPrice) }}</span>
+        <span class="price">{{ formatPrice(product.price) }}</span>
+      </div>
+
+      <p class="installment">10x de {{ formatPrice(installment) }} sem juros</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductCard',
+  props: {
+    product: Object
+  },
+  computed: {
+    installment () {
+      return this.product.price / 10
+    }
+  },
+  methods: {
+    formatPrice (value) {
+      return value.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      })
+    },
+  }
+}
+</script>
