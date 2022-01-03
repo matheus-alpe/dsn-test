acaf929cd88936700138e7c7f5b7ee95b2ac7fe6
Matheus Pereira <matttalves@gmail.com>
2022-01-03 11:47:14 -0300

    feat: flags and unavailable message

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="product" :class="{ 'unavailable': !product.available }" >
     <div class="info">
+      <div
+        class="flags"
+        v-if="product.available"
+      >
+        <span
+          class="flag discount"
+          v-if="discount"
+        >
+          {{ discount }}% OFF
+        </span>
+
+        <span class="flag free-shipping">Frete Grátis</span>
+      </div>
       <div class="thumbnail">
         <img
           :src="product.image_url"
@@ -12,13 +25,30 @@
       <div class="content">
         <p class="name">{{ product.name }}</p>
 
-        <div class="prices">
-          <span class="oldprice">{{ formatPrice(product.oldPrice) }}</span>
-          <span class="price">{{ formatPrice(product.price) }}</span>
-        </div>
+        <template v-if="product.available">
+          <div class="prices">
+            <div class="oldPrice-container">
+              <span
+                class="oldprice" 
+                v-if="discount"
+              >
+                {{ formatPrice(product.oldPrice) }}
+              </span>
+            </div>
 
-        <p class="installment">10x de {{ formatPrice(installment) }} sem juros</p>
-        <a :href="product.image_url" target="_blank" class="view">Ver produto</a>
+            <span class="price">{{ formatPrice(product.price) }}</span>
+          </div>
+
+          <p class="installment">10x de {{ formatPrice(installment) }} sem juros</p>
+          <a :href="product.image_url" target="_blank" class="view">Ver produto</a>
+        </template>
+
+        <div
+          v-else
+          class="unavailable-message"
+        >
+          <p>Produto Indisponível</p>
+        </div>
       </div>
     </div>
     <a :href="product.image_url" target="_blank" class="view overlay">Ver produto</a>
@@ -35,6 +65,12 @@ export default {
     installment() {
       return this.product.price / 10;
     },
+    
+    discount() {
+      const discount = this.product.price / this.product.oldPrice;
+      if (discount === 1) return false;
+      return Math.trunc((1 - discount) * 100);
+    }
   },
   methods: {
     formatPrice(value) {
